7a7a4eaa12210bbc8ea44d4b169201fb2b78aa0b
Paul M Furley <paul@paulfurley.com>
2018-08-14 12:22:20 +0100

    Add gpgwrapper module with a unit test

---
+++ gpgwrapper/gpgwrapper.go
@@ -0,0 +1,53 @@
+// gpgwrapper calls out to the system GnuPG binary
+
+package gpgwrapper
+
+import (
+	"errors"
+	"os/exec"
+	"regexp"
+)
+
+const GpgPath = "gpg"
+
+var VersionRegexp = regexp.MustCompile(`gpg \(GnuPG.*\) (\d+\.\d+\.\d+)`)
+
+func Version() (string, error) {
+	// Returns the GnuPG version string, e.g. "1.2.3"
+
+	outString, err := runGpg("--version")
+
+	if err != nil {
+		return "", err
+	}
+
+	version, err := parseVersionString(outString)
+
+	if err != nil {
+		return "", err
+	}
+
+	return version, nil
+}
+
+func parseVersionString(gpgStdout string) (string, error) {
+	match := VersionRegexp.FindStringSubmatch(gpgStdout)
+
+	if match == nil {
+		return "", errors.New("version string not found in GPG output")
+	}
+
+	return match[1], nil
+}
+
+func runGpg(arguments string) (string, error) {
+	out, err := exec.Command(GpgPath, "--version").Output()
+
+	if err != nil {
+		// TODO: it would be kinder if we interpreted GPG's
+		// output and returned a specific Error type.
+		return "", err
+	}
+	outString := string(out)
+	return outString, nil
+}
