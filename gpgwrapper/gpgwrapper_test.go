272b508b662146bfbd9329457463c4b9d89d2ec8
Ian Drysdale <idrysdale@gmail.com>
2018-08-17 16:12:25 +0100

    Refactor gpgwrapper_test for clarity

--- gpgwrapper/gpgwrapper_test.go
+++ gpgwrapper/gpgwrapper_test.go
@@ -1,35 +1,56 @@
 package gpgwrapper
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestParseGPGOutputVersion(t *testing.T) {
-	assert_parses_equal(t, "foo\ngpg (GnuPG) 2.2.4\nbar", "2.2.4")
-	assert_parses_equal(t, "foo\ngpg (GnuPG/MacGPG2) 2.2.8\nbar", "2.2.8")
-	assert_parses_equal(t, "foo\ngpg (GnuPG/MacGPG2) 111.222.333\nbar", "111.222.333")
+
+	t.Run("test GPG output from Ubuntu", func(t *testing.T) {
+		gpgOutput := "foo\ngpg (GnuPG) 2.2.4\nbar"
+		assert_parses_version_correctly(t, gpgOutput, "2.2.4")
+	})
+
+	t.Run("test GPG output from macOS", func(t *testing.T) {
+		gpgOutput := "foo\ngpg (GnuPG/MacGPG2) 2.2.8\nbar"
+		assert_parses_version_correctly(t, gpgOutput, "2.2.8")
+	})
+
+	t.Run("test long version numbers", func(t *testing.T) {
+		gpgOutput := "foo\ngpg (GnuPG/MacGPG2) 111.222.333\nbar"
+		assert_parses_version_correctly(t, gpgOutput, "111.222.333")
+	})
+
+	t.Run("test output not containing a version number", func(t *testing.T) {
+		gpgOutput := "foo\ngpg\nbar"
+		_, err := parseVersionString(gpgOutput)
+		want := errors.New("version string not found in GPG output")
+		assertError(t, err, want)
+	})
 }
 
-func TestReturnsErrorForBadGpgOutput(t *testing.T) {
-	_, err := parseVersionString("bad output")
-
-	if err == nil {
-		t.Errorf("Test failed, no error returned")
-	}
-
-	expectedError := "version string not found in GPG output"
-
-	if err.Error() != expectedError {
-		t.Errorf("Test failed, expected error %s, got '%s'", expectedError, err.Error())
-	}
-}
-
-func assert_parses_equal(t *testing.T, gpgOutput string, expected string) {
-	actual, err := parseVersionString(gpgOutput)
+func assert_parses_version_correctly(t *testing.T, gpgOutput string, want string) {
+	t.Helper()
+	got, err := parseVersionString(gpgOutput)
 
 	if err != nil {
 		t.Errorf("Test failed, returned error %s", err)
 	}
 
-	if actual != expected {
-		t.Errorf("Test failed, expected '%s', got '%s'", expected, actual)
+	if got != want {
+		t.Errorf("Test failed, expected '%s', got '%s'", want, got)
+	}
+}
+
+func assertError(t *testing.T, got error, want error) {
+	t.Helper()
+
+	if got == nil {
+		t.Fatal("wanted an error but didnt get one")
+	}
+
+	if got != want {
+		t.Errorf("wanted '%s', got '%s'", got, want)
 	}
 }
