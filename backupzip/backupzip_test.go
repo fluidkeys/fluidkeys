a1a9540a713ff5ae9c28d341e2921d6b759ccba5
Paul M Furley <paul@paulfurley.com>
2018-09-13 12:13:36 +0100

    Use assert.AssertEqualSliceOfStrings in backupzip

--- backupzip/backupzip_test.go
+++ backupzip/backupzip_test.go
@@ -5,6 +5,8 @@ import (
 	"bytes"
 	"io"
 	"testing"
+
+	"github.com/fluidkeys/fluidkeys/assert"
 )
 
 func TestMakeBackupFile(t *testing.T) {
@@ -31,7 +33,7 @@ func TestMakeBackupFile(t *testing.T) {
 			"2018-01-15-test-example-com-FAKEFINGERPRINT.revoke.txt",
 		}
 
-		assertStringSlicesEqual(t, wantFilenames, gotFilenames)
+		assert.AssertEqualSliceOfStrings(t, wantFilenames, gotFilenames)
 	})
 
 	t.Run("contents of ZIP file is correct", func(t *testing.T) {
@@ -57,21 +59,6 @@ func TestMakeBackupFile(t *testing.T) {
 		assertEqual(t, "/tmp/.foo/backups/2018-01-15-test-example-com-FAKEFINGERPRINT.zip", getZipFilename("/tmp/.foo", exampleSlug))
 	})
 
-}
-
-func assertStringSlicesEqual(t *testing.T, want []string, got []string) {
-	t.Helper()
-	if len(want) != len(got) {
-		t.Fatalf("not equal: want: `%v`, got `%v`", want, got)
-	}
-
-	for i := range want {
-
-		if want[i] != got[i] {
-			t.Fatalf("not equal, want[%d]=`%v`, got[%d]=`%v`",
-				i, want[i], i, got[i])
-		}
-	}
 }
 
 func assertEqual(t *testing.T, want string, got string) {
