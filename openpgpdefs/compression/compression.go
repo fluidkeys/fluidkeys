993e9ed6b16dc0448c24dbb4e0a734f1c54b2ce5
Paul Fawkesley <paul@fluidkeys.com>
2018-10-08 12:29:04 +0100

    add compression.Name function

--- openpgpdefs/compression/compression.go
+++ openpgpdefs/compression/compression.go
@@ -1,5 +1,9 @@
 package compression
 
+import (
+	"fmt"
+)
+
 type CompressionAlgorithm = uint8
 
 const (
@@ -9,3 +13,26 @@ const (
 	ZLIB                              = 2
 	BZIP2                             = 3
 )
+
+func Name(compressionAlgorithm CompressionAlgorithm) string {
+	switch compressionAlgorithm {
+	case Uncompressed:
+		return "Uncompressed"
+
+	case ZIP:
+		return "ZIP"
+
+	case ZLIB:
+		return "ZLIP"
+
+	case BZIP2:
+		return "BZIP2"
+
+	case 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110:
+		return fmt.Sprintf("Private/experimental (%d)", compressionAlgorithm)
+
+	default:
+		return fmt.Sprintf("Unknown (%d)", compressionAlgorithm)
+	}
+
+}
