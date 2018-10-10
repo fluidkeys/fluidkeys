235c3d35c3b1584eb08ce4b00a7fad462ccb2212
Paul Fawkesley <paul@fluidkeys.com>
2018-10-10 10:46:19 +0100

    tweak comments on existing KeyAction types

--- status/keyactions.go
+++ status/keyactions.go
@@ -10,8 +10,9 @@ import (
 	"github.com/fluidkeys/fluidkeys/pgpkey"
 )
 
-// ModifyPrimaryKeyExpiry means the self signature of the key will be refreshed
-// with a future expiry date
+// ModifyPrimaryKeyExpiry iterates over all user IDs. For each UID, it updates
+// the expiry date on the *self signature*.
+// It re-signs the self signature.
 type ModifyPrimaryKeyExpiry struct {
 	KeyAction
 
@@ -19,8 +20,6 @@ type ModifyPrimaryKeyExpiry struct {
 	PreviouslyValidUntil *time.Time
 }
 
-// ModifyPrimaryKeyExpiry creates a new self signature on each user ID with
-// ValidUntil
 func (a ModifyPrimaryKeyExpiry) Enact(key *pgpkey.PgpKey) error {
 	return key.UpdateExpiryForAllUserIds(a.ValidUntil)
 }
@@ -33,15 +32,14 @@ func (a ModifyPrimaryKeyExpiry) String() string {
 	}
 }
 
-// CreateNewEncryptionSubkey describes creating a new subkey with the given
-// ValidUntil expiry time.
+// CreateNewEncryptionSubkey creates a new subkey with the given
+// ValidUntil expiry time and a subkey binding signature.
 type CreateNewEncryptionSubkey struct {
 	KeyAction
 
 	ValidUntil time.Time
 }
 
-// Enact calls PgpKey.CreateNewEncryptionSubkey()
 func (a CreateNewEncryptionSubkey) Enact(key *pgpkey.PgpKey) error {
 	return key.CreateNewEncryptionSubkey(a.ValidUntil)
 }
