c580d8c166052a2faa65fbbf6d9bcbecbbd65172
Paul Fawkesley <paul@fluidkeys.com>
2018-10-10 10:41:45 +0100

    add 5 KeyAction types (without implementation)
    * SetPreferredSymmetricAlgorithms
    * SetPreferredHashAlgorithms
    * SetPreferredCompressionAlgorithms
    * RefreshUserIdSelfSignatures
    * RefreshSubkeyBindingSignature

--- status/keyactions.go
+++ status/keyactions.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/fluidkeys/fluidkeys/openpgpdefs/compression"
+	"github.com/fluidkeys/fluidkeys/openpgpdefs/hash"
+	"github.com/fluidkeys/fluidkeys/openpgpdefs/symmetric"
 	"github.com/fluidkeys/fluidkeys/pgpkey"
 )
 
@@ -64,6 +67,79 @@ func (a ExpireSubkey) String() string {
 	return fmt.Sprintf("Expire the encryption subkey now (0x%X)", a.SubkeyId)
 }
 
+// SetPreferredSymmetricAlgorithms iterates over all user IDs, setting the preferred
+// symmetric algorithm preferences from NewPreferences
+// It re-signs the self signature on each user ID.
+type SetPreferredSymmetricAlgorithms struct {
+	KeyAction
+
+	NewPreferences []symmetric.SymmetricAlgorithm
+}
+
+func (a SetPreferredSymmetricAlgorithms) Enact(key *pgpkey.PgpKey) error {
+	return fmt.Errorf("not implemented")
+}
+
+func (a SetPreferredSymmetricAlgorithms) String() string {
+	return fmt.Sprintf("Set preferred encryption algorithms to %s", joinCipherNames(a.NewPreferences))
+}
+
+// SetPreferredHashAlgorithms iterates over all user IDs, setting the preferred
+// hash algorithm preferences from NewPreferences
+// It re-signs the self signature on each user ID.
+type SetPreferredHashAlgorithms struct {
+	KeyAction
+
+	NewPreferences []hash.HashAlgorithm
+}
+
+func (a SetPreferredHashAlgorithms) Enact(key *pgpkey.PgpKey) error {
+	return fmt.Errorf("not implemented")
+}
+
+func (a SetPreferredHashAlgorithms) String() string {
+	return fmt.Sprintf("Set preferred hash algorithms to %s", joinHashNames(a.NewPreferences))
+}
+
+// SetPreferredCompressionAlgorithms iterates over all user IDs, setting the preferred
+// compression algorithm preferences from NewPreferences.
+// It re-signs the self signature on each user ID.
+type SetPreferredCompressionAlgorithms struct {
+	KeyAction
+
+	NewPreferences []compression.CompressionAlgorithm
+}
+
+func (a SetPreferredCompressionAlgorithms) Enact(key *pgpkey.PgpKey) error {
+	return fmt.Errorf("not implemented")
+}
+
+func (a SetPreferredCompressionAlgorithms) String() string {
+	return fmt.Sprintf("Set preferred compression algorithms to %s", joinCompressionNames(a.NewPreferences))
+}
+
+type RefreshUserIdSelfSignatures struct {
+	KeyAction
+}
+
+func (a RefreshUserIdSelfSignatures) Enact(key *pgpkey.PgpKey) error {
+	return fmt.Errorf("not implemented")
+}
+func (a RefreshUserIdSelfSignatures) String() string {
+	return "Create new self signatures"
+}
+
+type RefreshSubkeyBindingSignature struct {
+	KeyAction
+	SubkeyId uint64
+}
+
+func (a RefreshSubkeyBindingSignature) Enact(key *pgpkey.PgpKey) error {
+	return fmt.Errorf("not implemented")
+}
+func (a RefreshSubkeyBindingSignature) String() string {
+	return fmt.Sprintf("Create new signature for subkey 0x%X", a.SubkeyId)
+}
 
 type KeyAction interface {
 	String() string
