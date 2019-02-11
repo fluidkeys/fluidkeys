4131f1378f6bf44196bd068438ecc1741d99add4
Paul Fawkesley <paul@fluidkeys.com>
2019-02-11 11:07:59 +0000

    more tests & fixes for formatFileDivider

--- fk/ui_test.go
+++ fk/ui_test.go
@@ -1,6 +1,7 @@
 package fk
 
 import (
+	"strings"
 	"testing"
 	"unicode/utf8"
 
@@ -41,5 +42,66 @@ func TestFormatFileDivider(t *testing.T) {
 			divider,
 		)
 		assert.Equal(t, 80, utf8.RuneCountInString(colour.StripAllColourCodes(divider)))
+
+		t.Run("with no file extension", func(t *testing.T) {
+			divider := formatFileDivider("example1234567890123456789012345678901234567890123456789012345678901234567890")
+			assert.Equal(
+				t,
+				"── "+colour.File("example123456789012345678901234567890123456789012345678901234567890123456…")+" ──",
+				divider,
+			)
+			assert.Equal(t, 80, utf8.RuneCountInString(colour.StripAllColourCodes(divider)))
+		})
+
+		t.Run("filename is 1 less than maxFilenameLength", func(t *testing.T) {
+			// should not be truncated
+			filename := strings.Repeat("a", maxFilenameLength-1)
+			divider := formatFileDivider(filename)
+
+			assert.Equal(
+				t,
+				"── "+colour.File(filename)+" ───", // note: 3 runes on right
+				divider,
+			)
+			assert.Equal(t, 80, utf8.RuneCountInString(colour.StripAllColourCodes(divider)))
+		})
+
+		t.Run("filename is exactly maxFilenameLength", func(t *testing.T) {
+			// should not be truncated
+			filename := strings.Repeat("a", maxFilenameLength)
+			divider := formatFileDivider(filename)
+
+			assert.Equal(
+				t,
+				"── "+colour.File(filename)+" ──",
+				divider,
+			)
+			assert.Equal(t, 80, utf8.RuneCountInString(colour.StripAllColourCodes(divider)))
+		})
+
+		t.Run("filename is 1 more than maxFilenameLength", func(t *testing.T) {
+			filename := strings.Repeat("a", maxFilenameLength+1)
+			divider := formatFileDivider(filename)
+
+			assert.Equal(
+				t,
+				"── "+colour.File(filename[0:maxFilenameLength-1]+"…")+" ──",
+				divider,
+			)
+			assert.Equal(t, 80, utf8.RuneCountInString(colour.StripAllColourCodes(divider)))
+		})
+
+		t.Run("filename is 100 more than maxFilenameLength", func(t *testing.T) {
+			filename := strings.Repeat("a", maxFilenameLength+100)
+			divider := formatFileDivider(filename)
+
+			assert.Equal(
+				t,
+				"── "+colour.File(filename[0:maxFilenameLength-1]+"…")+" ──",
+				divider,
+			)
+			assert.Equal(t, 80, utf8.RuneCountInString(colour.StripAllColourCodes(divider)))
+		})
 	})
+
 }
