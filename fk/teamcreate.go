066c225936962de9819407b859dae0f194527edf
Ian Drysdale <ian@fluidkeys.com>
2019-02-21 12:26:19 +0000

    team create: prompt for which team email address to use

--- fk/teamcreate.go
+++ fk/teamcreate.go
@@ -19,10 +19,13 @@ package fk
 
 import (
 	"fmt"
+	"strconv"
 	"unicode/utf8"
 
+	"github.com/fluidkeys/fluidkeys/colour"
 	"github.com/fluidkeys/fluidkeys/emailutils"
 	"github.com/fluidkeys/fluidkeys/out"
+	"github.com/fluidkeys/fluidkeys/pgpkey"
 	"github.com/fluidkeys/fluidkeys/stringutils"
 	"github.com/fluidkeys/fluidkeys/ui"
 )
@@ -35,13 +38,51 @@ func teamCreate() exitCode {
 	out.Print("This makes it easy to send secrets to one another and use other popular\n")
 	out.Print("PGP tools.\n\n")
 
+	printHeader("Which is your team email address?")
+
+	keys, err := loadPgpKeys()
+	if err != nil {
+		out.Print(ui.FormatFailure("Error loading pgp keys", nil, err))
+		return 1
+	}
+
+	if len(keys) == 0 {
+		out.Print(ui.FormatWarning(
+			"No keys found in fluidkeys", []string{
+				"Before creating a team you must create a key by running ",
+				"    " + colour.Cmd("fk setup"),
+			},
+			nil,
+		))
+		return 1
+	}
+
+	for index, key := range keys {
+		email, err := key.Email()
+		if err != nil {
+			out.Print(ui.FormatFailure(
+				"Failed to get email for key", []string{
+					fmt.Sprintf("%s has no identities with email addresses", key.Fingerprint()),
+				},
+				err,
+			))
+		}
+		formattedListNumber := colour.Info(fmt.Sprintf("%-4s", (strconv.Itoa(index+1) + ".")))
+		out.Print(fmt.Sprintf("%s%s\n", formattedListNumber, email))
+	}
+
+	out.Print("\n")
+	out.Print("If your team email address isn't listed, quit and run " +
+		colour.Cmd("fk key create") + "\n")
+	out.Print("\n")
+	key := promptForTeamEmail(keys)
+
 	printHeader("What's your team name?")
 
 	out.Print("This is how your team will be displayed to you and other members. You can\n")
 	out.Print("always change this later.\n\n")
 
 	var teamName string
-	var err error
 
 	for teamName == "" {
 		teamName, err = validateTeamName(promptForInput("[team name] : "))
@@ -72,8 +113,9 @@ func teamCreate() exitCode {
 
 	printHeader("Finishing setup")
 
-	out.Print("You've indicated you want to invite the following members to " +
-		teamName + ":\n\n")
+	out.Print("You've indicated you want setup " + teamName + " using your key\n")
+	out.Print(fmt.Sprintf("%s\n", key.Fingerprint()))
+	out.Print("With the following members:\n\n")
 	for _, memberEmail := range teamMemberEmails {
 		out.Print(memberEmail + "\n")
 	}
@@ -93,4 +135,33 @@ func validateTeamName(teamName string) (string, error) {
 		return "", fmt.Errorf("Team name contained invalid characters")
 	}
 	return teamName, nil
+}
+
+func promptForTeamEmail(keys []pgpkey.PgpKey) *pgpkey.PgpKey {
+	var selectedKey int
+	if len(keys) == 1 {
+		onlyKey := keys[0]
+		prompter := interactiveYesNoPrompter{}
+		if prompter.promptYesNo("Is this your team email?", "y", nil) {
+			return &onlyKey
+		}
+		return nil
+	} else {
+		invalidEntry := fmt.Sprintf("Please select between 1 and %v.\n", len(keys))
+		for validInput := false; !validInput; {
+			rangePrompt := colour.Info(fmt.Sprintf("[1-%v]", len(keys)))
+			input := promptForInput(fmt.Sprintf(promptWhichKeyFromGPG + " " + rangePrompt + " "))
+			if integerSelected, err := strconv.Atoi(input); err != nil {
+				out.Print(invalidEntry)
+			} else {
+				if (integerSelected >= 1) && (integerSelected <= len(keys)) {
+					selectedKey = integerSelected - 1
+					validInput = true
+				} else {
+					out.Print(invalidEntry)
+				}
+			}
+		}
+		return &keys[selectedKey]
+	}
 }
