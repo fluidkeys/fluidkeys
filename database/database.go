fluidkeys/fluidkeys

commit 142afe9ce6ac35d14f7fe66c31f83dc8be9eafae

add db.GetExistingRequestToJoinTeam(teamUUID, fpr)

database/database.go
@@ -141,6 +141,31 @@ func (db *Database) GetRequestsToJoinTeams() (requests []team.RequestToJoinTeam,
 	return requests, nil
 }
 
+func (db *Database) GetExistingRequestToJoinTeam(teamUUID uuid.UUID, fingerprint fpr.Fingerprint) (
+	request *team.RequestToJoinTeam, err error) {
+
+	message, err := db.loadFromFile()
+	if os.IsNotExist(err) {
+		return nil, ErrRequestNotFound
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	for _, msg := range message.RequestsToJoinTeams {
+		if msg.TeamUUID == teamUUID && msg.Fingerprint == fingerprint {
+			return &team.RequestToJoinTeam{
+				TeamUUID:    msg.TeamUUID,
+				TeamName:    msg.TeamName,
+				Fingerprint: msg.Fingerprint,
+				RequestedAt: msg.RequestedAt,
+			}, nil
+		}
+	}
+
+	return nil, ErrRequestNotFound
+}
+
 // DeleteRequestToJoinTeam deletes all requests to join the team matching the given team UUID and
 // fingerprint.
 func (db *Database) DeleteRequestToJoinTeam(teamUUID uuid.UUID, fingerprint fpr.Fingerprint) error {
@@ -214,3 +239,7 @@ func deduplicateKeyImportedIntoGnuPGMessages(slice []KeyImportedIntoGnuPGMessage
 	}
 	return deduped
 }
+
+var (
+	ErrRequestNotFound = fmt.Errorf("No request to join team with that fingerprint found")
+)

database/database_test.go
@@ -251,7 +251,63 @@ func TestDeleteRequestToJoinTeam(t *testing.T) {
 		expectedRequests := []team.RequestToJoinTeam{req3, req4}
 		assert.Equal(t, expectedRequests, gotRequests)
 	})
+}
 
+func TestGetExistingRequestToJoinTeam(t *testing.T) {
+	now := time.Date(2019, 6, 20, 16, 35, 0, 0, time.UTC)
+
+	team1UUID := uuid.Must(uuid.NewV4())
+
+	request1 := team.RequestToJoinTeam{
+		TeamUUID:    team1UUID,
+		UUID:        uuid.UUID{}, // empty *request* UUID, we don't store that
+		Fingerprint: exampleFingerprintA,
+		RequestedAt: now,
+		Email:       "",
+	}
+
+	request2 := team.RequestToJoinTeam{
+		TeamUUID:    team1UUID,
+		UUID:        uuid.UUID{}, // empty *request* UUID, we don't store that
+		Fingerprint: exampleFingerprintB,
+		RequestedAt: now,
+		Email:       "",
+	}
+
+	team2UUID := uuid.Must(uuid.NewV4())
+
+	request3 := team.RequestToJoinTeam{
+		TeamUUID:    team2UUID,
+		UUID:        uuid.UUID{}, // empty *request* UUID, we don't store that
+		Fingerprint: exampleFingerprintA,
+		RequestedAt: now,
+		Email:       "",
+	}
+
+	t.Run("gets the request for the matching teamUUID and fingerprint", func(t *testing.T) {
+		database := New(makeTempDirectory(t))
+
+		addRequestToJoinToDatabase(t, request1, database)
+		addRequestToJoinToDatabase(t, request2, database)
+		addRequestToJoinToDatabase(t, request3, database)
+
+		gotRequest, err := database.GetExistingRequestToJoinTeam(team1UUID, exampleFingerprintA)
+		assert.NoError(t, err)
+		assert.Equal(t, request1, *gotRequest)
+	})
+
+	t.Run("gets a specific error when request can't be found", func(t *testing.T) {
+		database := New(makeTempDirectory(t))
+
+		addRequestToJoinToDatabase(t, request1, database)
+
+		_, err := database.GetExistingRequestToJoinTeam(
+			uuid.Must(uuid.NewV4()),
+			exampleFingerprintA,
+		)
+		assert.GotError(t, err)
+		assert.Equal(t, ErrRequestNotFound, err)
+	})
 }
 
 func TestDeduplicateKeyImportedIntoGnuPGMessages(t *testing.T) {
@@ -273,6 +329,16 @@ func TestDeduplicateKeyImportedIntoGnuPGMessages(t *testing.T) {
 	if len(got) != len(want) {
 		t.Errorf("Expected '%v' but got '%v'", want, got)
 	}
+}
+
+func addRequestToJoinToDatabase(t *testing.T, request team.RequestToJoinTeam, database Database) {
+	t.Helper()
+	assert.NoError(t, database.RecordRequestToJoinTeam(
+		request.TeamUUID,
+		request.TeamName,
+		request.Fingerprint,
+		request.RequestedAt,
+	))
 }
 
 func makeTempDirectory(t *testing.T) string {

commit d1d2ead115ad13712e9ab7f12027ad941a9fa2f8

fix Golint errors introduced in 142afe9c
note that error strings should begin with lowecase

database/database.go
@@ -141,6 +141,7 @@ func (db *Database) GetRequestsToJoinTeams() (requests []team.RequestToJoinTeam,
 	return requests, nil
 }
 
+// GetExistingRequestToJoinTeam returns a single request based on the teamUUID and fingerprint.
 func (db *Database) GetExistingRequestToJoinTeam(teamUUID uuid.UUID, fingerprint fpr.Fingerprint) (
 	request *team.RequestToJoinTeam, err error) {
 
@@ -241,5 +242,7 @@ func deduplicateKeyImportedIntoGnuPGMessages(slice []KeyImportedIntoGnuPGMessage
 }
 
 var (
-	ErrRequestNotFound = fmt.Errorf("No request to join team with that fingerprint found")
+	// ErrRequestNotFound is returned when a request to join a team is not found matching the given
+	// team UUID and fingerprint
+	ErrRequestNotFound = fmt.Errorf("no request to join team with that fingerprint found")
 )