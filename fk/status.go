5d9c05925457c436eb20482d426173d7bb2d48db
Ian Drysdale <ian@fluidkeys.com>
2019-03-22 15:25:55 +0000

    factor out printMemberships in status

--- fk/status.go
+++ fk/status.go
@@ -33,6 +33,14 @@ import (
 func statusSubcommand(args docopt.Opts) exitCode {
 	out.Print("\n")
 
+	if code := printMemberships(); code != 0 {
+		return code
+	}
+
+	return 0
+}
+
+func printMemberships() exitCode {
 	groupedMemberships, err := user.GroupedMemberships()
 	if err != nil {
 		log.Panic(err)
