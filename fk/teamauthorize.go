b29ee9a5cd247556267c7d1c8cc2ea5b3603d161
Paul Fawkesley <paul@fluidkeys.com>
2019-03-25 13:30:25 +0000

    add intro line to roster

--- fk/teamauthorize.go
+++ fk/teamauthorize.go
@@ -88,6 +88,7 @@ func teamAuthorize() exitCode {
 					})
 			}
 
+			out.Print("The team roster is a signed file that defines who is in the team.\n\n")
 			if err := promptAndSignAndUploadRoster(myTeam, &adminKey); err != nil {
 				out.Print(ui.FormatFailure("Failed to sign and upload roster", nil, err))
 				return 1
