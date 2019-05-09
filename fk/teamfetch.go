fluidkeys/fluidkeys

commit 451c6182f698b5a0a7ef6dd81480e358c9ecc364

split fetch, sign, import into 3 checkboxes
before:

```
Fetching and signing keys for other members of Tina's new gang:

     [✔] mark@example.com

│ ✔ Successfully fetched keys and imported them into GnuPG

```

after:

```
Fetching and signing keys for other members of Tina's new gang:

     [✔] mark@example.com: fetch key
     [✔] mark@example.com: sign key
     [✔] mark@example.com: import into gpg

│ ✔ Successfully fetched keys and imported them into GnuPG

```

although it's wordier, it helps to see what's going on, which is
reassuring if it's about to prompt you for a password.

fk/teamfetch.go
@@ -178,8 +178,10 @@ func fetchAndCertifyTeamKeys(
 			}
 		}
 
-		err = ui.RunWithCheckboxes(person.Email, func() error {
-			key, err := api.GetPublicKeyByFingerprint(person.Fingerprint)
+		var theirKey *pgpkey.PgpKey
+
+		err = ui.RunWithCheckboxes(person.Email+": fetch key", func() error {
+			theirKey, err = api.GetPublicKeyByFingerprint(person.Fingerprint)
 
 			if err != nil && err == apiclient.ErrPublicKeyNotFound {
 				log.Print(err)
@@ -188,6 +190,10 @@ func fetchAndCertifyTeamKeys(
 				log.Print(err)
 				return fmt.Errorf("Got error from Fluidkeys server")
 			}
+			return nil
+		})
+
+		err = ui.RunWithCheckboxes(person.Email+": sign key", func() error {
 
 			if !alreadyCertified(person.Email, person.Fingerprint, me.Fingerprint) {
 				unlockedKey, err := getUnlockedKey(me.Fingerprint, unattended)
@@ -199,7 +205,7 @@ func fetchAndCertifyTeamKeys(
 					return err
 				}
 
-				if err := key.CertifyEmail(person.Email, unlockedKey, time.Now()); err != nil {
+				if err := theirKey.CertifyEmail(person.Email, unlockedKey, time.Now()); err != nil {
 					log.Print(err)
 					return fmt.Errorf("Failed to sign key: %v", err)
 				}
@@ -208,9 +214,13 @@ func fetchAndCertifyTeamKeys(
 			} else {
 				log.Printf("key %s already certified by %s, not certifying again",
 					person.Fingerprint.Hex(), me.Fingerprint.Hex())
+				return ui.SkipThisAction
 			}
+			return nil
+		})
 
-			armoredKey, err := key.Armor()
+		err = ui.RunWithCheckboxes(person.Email+": import into gpg", func() error {
+			armoredKey, err := theirKey.Armor()
 			if err != nil {
 				log.Print(err)
 				return fmt.Errorf("failed to ASCII armor key")
@@ -221,7 +231,7 @@ func fetchAndCertifyTeamKeys(
 				log.Print(err)
 				return fmt.Errorf("Failed to import key into gpg")
 			}
-			db.RecordLast("fetch", key.Fingerprint(), time.Now())
+			db.RecordLast("fetch", theirKey.Fingerprint(), time.Now())
 
 			return nil
 		})

commit bfc985a97777bfbefb526cc8575d24dab5c134e2

team fetch: don't continue if failed to fetch key
fail more gracefully than a nil pointer error

https://github.com/fluidkeys/fluidkeys/issues/502

this bug was introduced in 451c6182f698b5a0a7ef6dd81480e358c9ecc364

fk/teamfetch.go
@@ -210,6 +210,9 @@ func fetchAndCertifyTeamKeys(
 			}
 			return nil
 		})
+		if err != nil {
+			continue
+		}
 
 		err = ui.RunWithCheckboxes(person.Email+": sign key", func() error {
 