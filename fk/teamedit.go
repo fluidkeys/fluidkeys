c798f3d86304ca2ff049f9f0ddbacfdecc6765f7
Paul Fawkesley <paul@fluidkeys.com>
2019-05-10 16:54:30 +0100

    add getEditor function
    getEditor reads the user's VISUAL or EDITOR environment variable (in that order) or "vi"
    if neither is set it returns the default "vi"

--- fk/teamedit.go
+++ fk/teamedit.go
@@ -1,6 +1,8 @@
 package fk
 
 import (
+	"os"
+
 	"github.com/fluidkeys/fluidkeys/out"
 	"github.com/fluidkeys/fluidkeys/ui"
 )
@@ -8,4 +10,30 @@ import (
 func teamEdit() exitCode {
 	out.Print(ui.FormatFailure("not implemented", nil, nil))
 	return 1
+}
+
+// getEditor reads the user's VISUAL or EDITOR environment variable (in that order) or "vi"
+// if neither is set it returns the default "vi"
+func getEditor() string {
+	const defaultEditor = "vi"
+
+	visual := os.Getenv("VISUAL")
+	if visual != "" {
+		return visual
+	}
+
+	editor := os.Getenv("EDITOR")
+	if editor != "" {
+		return editor
+	}
+
+	out.Print(ui.FormatInfo(
+		"Using default editor `"+defaultEditor+"`",
+		[]string{
+			"You can change this by setting the EDITOR environment variable:",
+			"",
+			"> export EDITOR=\"nano\"",
+		}))
+
+	return defaultEditor
 }
